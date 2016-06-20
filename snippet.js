b3022e5df81cfebb997a1a1bb9d57c9ae8c343e2
Jeff Stephens <jefftheman45@gmail.com>
2016-06-19 23:51:42 -0600

    First working version of snippet

---
+++ snippet.js
@@ -0,0 +1,40 @@
+/**
+ * Tumblr Welcome Page
+ *
+ * This JS snippet should be installed in your Tumblr theme. Customize the
+ * redirect URL at the top. Every new browser session, this will redirect
+ * users from your homepage to the URL you specify here.
+ */
+
+URL_TO_REDIRECT_TO = 'http://example.com/welcome-page/';
+
+function atBaseUrl() {
+	return window.location.pathname == "/";
+}
+
+// source: https://mathiasbynens.be/notes/localstorage-pattern
+var hasStorage = (function() {
+	try {
+		sessionStorage.setItem('mod', 'mod');
+		sessionStorage.removeItem('mod');
+		return true;
+	} catch (exception) {
+		return false;
+	}
+}());
+
+// make sure we support sessionStorage, otherwise do nothing
+if (hasStorage) {
+
+	// make sure we're at the root of the current page, otherwise do nothing
+	if (atBaseUrl()) {
+
+		// if we don't have the sessionStorage entry, create it and redirect
+		if (sessionStorage.getItem('visited') === null) {
+			sessionStorage.setItem('visited', 'true');
+			window.location=URL_TO_REDIRECT_TO;
+		}
+
+	}
+
+}
